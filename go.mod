45d8785423afb4f1734b7d2612194477c8e01d81
Richard Holzeis <richard.holzeis@at.ibm.com>
2020-01-04 13:55:54 +0100

    created pubsub router

--- go.mod
+++ go.mod
@@ -4,8 +4,7 @@ go 1.13
 
 require (
 	github.com/libp2p/go-libp2p v0.5.0
-	github.com/libp2p/go-libp2p-autonat-svc v0.1.0 // indirect
-	github.com/libp2p/go-libp2p-connmgr v0.2.1 // indirect
-	github.com/libp2p/go-libp2p-kad-dht v0.4.1 // indirect
-	github.com/libp2p/go-libp2p-quic-transport v0.2.2 // indirect
+	github.com/libp2p/go-libp2p-core v0.3.0
+	github.com/libp2p/go-libp2p-pubsub v0.2.5
+	github.com/multiformats/go-multiaddr v0.2.0
 )
--- main.go
+++ main.go
@@ -3,22 +3,48 @@ package main
 import (
 	"context"
 	"fmt"
+	"math/rand"
 
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/crypto"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+
+	"github.com/multiformats/go-multiaddr"
 )
 
 func main() {
 	fmt.Println("startup")
 
-	// The context governs the lifetime of the libp2p node
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	sourcePort := 5555
 
-	// To construct a simple host with all the default settings, just use `New`
-	h, err := libp2p.New(ctx)
+	r := rand.New(rand.NewSource(int64(sourcePort)))
+
+	// Creates a new RSA key pair for this host.
+	prvKey, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("Hello World, my hosts ID is %s\n", h.ID())
+	sourceMultiAddr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", sourcePort))
+
+	host, err := libp2p.New(
+		context.Background(),
+		libp2p.ListenAddrs(sourceMultiAddr),
+		libp2p.Identity(prvKey),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	router, err := pubsub.NewGossipSub(context.Background(), host)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, peer := range router.ListPeers("") {
+		fmt.Println(peer.ShortString())
+	}
+
+	fmt.Printf("Hello World, my hosts ID is %s\n", host.ID())
+
 }
