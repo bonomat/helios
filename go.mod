fd89eb9ba08bf592b8bcca2ad0c467e485cac75d
Philipp Hoenisch <philipp@hoenisch.at>
2020-01-04 13:44:21 +0100

    Get localhost with default setting

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/bonomat/helios
 
 go 1.13
+
+require (
+	github.com/libp2p/go-libp2p v0.5.0
+	github.com/libp2p/go-libp2p-autonat-svc v0.1.0 // indirect
+	github.com/libp2p/go-libp2p-connmgr v0.2.1 // indirect
+	github.com/libp2p/go-libp2p-kad-dht v0.4.1 // indirect
+	github.com/libp2p/go-libp2p-quic-transport v0.2.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/libp2p/go-libp2p"
+)
 
 func main() {
 	fmt.Println("startup")
+
+	// The context governs the lifetime of the libp2p node
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// To construct a simple host with all the default settings, just use `New`
+	h, err := libp2p.New(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello World, my hosts ID is %s\n", h.ID())
 }
