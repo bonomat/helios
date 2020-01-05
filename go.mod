d1ae41b0e1240bc8e7ef553f8189e115fb435d65
Richard Holzeis <richard.holzeis@at.ibm.com>
2020-01-05 17:51:16 +0100

    added logging

--- go.mod
+++ go.mod
@@ -3,11 +3,16 @@ module github.com/bonomat/helios
 go 1.13
 
 require (
+	github.com/Shopify/toxiproxy v2.1.4+incompatible
 	github.com/btcsuite/btcd v0.20.1-beta
+	github.com/ipfs/go-log v0.0.1
 	github.com/libp2p/go-libp2p v0.5.0
 	github.com/libp2p/go-libp2p-core v0.3.0
 	github.com/libp2p/go-libp2p-crypto v0.1.0
 	github.com/libp2p/go-libp2p-kad-dht v0.4.1
+	github.com/libp2p/go-libp2p-protocol v0.1.0
 	github.com/libp2p/go-libp2p-pubsub v0.2.5
 	github.com/multiformats/go-multiaddr v0.2.0
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
+	github.com/whyrusleeping/go-logging v0.0.1
 )
--- main.go
+++ main.go
@@ -1,52 +1,79 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"crypto/ecdsa"
 	"crypto/rand"
 	"flag"
 	"fmt"
+
+	"os"
 	"sync"
 	"time"
+
+	"github.com/ipfs/go-log"
+	logging "github.com/whyrusleeping/go-logging"
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/libp2p/go-libp2p"
 	core "github.com/libp2p/go-libp2p-core"
 	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/protocol"
 	crypto "github.com/libp2p/go-libp2p-crypto"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/multiformats/go-multiaddr"
 )
 
-func startListening(ps libp2pPubSub, host core.Host) {
-	wg := &sync.WaitGroup{}
+var logger = log.Logger("helios")
 
-	ctx := context.Background()
+func startListening(ctx context.Context, ps libp2pPubSub, host core.Host) {
+	wg := &sync.WaitGroup{}
 
 	wg.Add(1)
 	go func(host *core.Host, pubSub *libp2pPubSub) {
-		for true {
+		for {
 			msg, err := pubSub.subscription.Next(ctx)
 			if err != nil {
-				fmt.Println(err.Error())
+				logger.Errorf("Error: %v\n", err)
+				continue
 			}
 
-			fmt.Printf("Node %s received Message: '%s' from '%s' \n", (*host).ID().Pretty(), string(msg.Data), string(msg.ReceivedFrom.Pretty()))
+			if (*host).ID() != msg.ReceivedFrom {
+				stream, err := (*host).NewStream(ctx, msg.ReceivedFrom, protocol.ID("/chat/1.1.0"))
+				if err != nil {
+					logger.Errorf("Error: %v\n", err)
+					continue
+				}
+				_, err = stream.Write([]byte("test"))
+				if err != nil {
+					logger.Errorf("Error: %v\n", err)
+					continue
+				}
+
+				rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+
+				go readData(rw)
+			}
+
+			logger.Infof("Node %s received Message: '%s' from '%s' \n", (*host).ID().Pretty(), string(msg.Data), string(msg.ReceivedFrom.Pretty()))
 		}
 	}(&host, &ps)
 
-	fmt.Println("Broadcasting a message ...")
+	logger.Infof("Broadcasting a message ...")
 	err := ps.pubsub.Publish("TOPIC", []byte("Bananaboatckr"))
 	if err != nil {
-		fmt.Println(err.Error())
-		return
+		logger.Errorf("Error: %v\n", err)
 	}
 
 	wg.Wait()
-	fmt.Println("The END")
+	logger.Infof("The END")
 }
 
 func main() {
+	log.SetAllLoggers(logging.WARNING)
+	log.SetLogLevel("helios", "info")
+
 	help := flag.Bool("h", false, "Display Help")
 	config, err := ParseFlags()
 	if err != nil {
@@ -54,7 +81,7 @@ func main() {
 	}
 
 	if *help {
-		fmt.Println("Usage: Run go run github.com/bonomat/helios in different terminals.")
+		logger.Infof("Usage: Run go run github.com/bonomat/helios in different terminals.")
 		flag.PrintDefaults()
 		return
 	}
@@ -66,7 +93,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(config.ListenAddresses[0].String() + "/p2p/" + host.ID().String())
+	logger.Infof(config.ListenAddresses[0].String() + "/p2p/" + host.ID().String())
 
 	ps := new(libp2pPubSub)
 	// creating pubsubs
@@ -76,17 +103,16 @@ func main() {
 	}
 
 	defer func() {
-		fmt.Println("Closing host")
+		logger.Info("Closing host")
 		host.Close()
+		ps.subscription.Cancel()
 	}()
 
 	for _, peer := range config.BootstrapPeers {
 		connectHostToPeer(ctx, host, peer)
-		// wait for network to be propagated
-		time.Sleep(time.Second * 2)
 	}
 
-	startListening(*ps, host)
+	startListening(ctx, *ps, host)
 }
 
 type libp2pPubSub struct {
@@ -98,7 +124,7 @@ type libp2pPubSub struct {
 // initializePubSub creates a PubSub for the peer and also subscribes to a topic
 func (c *libp2pPubSub) initializePubSub(ctx context.Context, host core.Host) (err error) {
 	optsPS := []pubsub.Option{
-		pubsub.WithMessageSigning(false),
+		pubsub.WithMessageSigning(true),
 	}
 
 	c.pubsub, err = pubsub.NewGossipSub(ctx, host, optsPS...)
@@ -149,12 +175,59 @@ func createHost(ctx context.Context, addresses addrList) (host core.Host, err er
 func connectHostToPeer(ctx context.Context, h core.Host, address multiaddr.Multiaddr) {
 	pInfo, err := peer.AddrInfoFromP2pAddr(address)
 	if err != nil {
-		fmt.Printf("Error : %v\n", err)
+		logger.Errorf("Error: %v\n", err)
 		return
 	}
 
 	err = h.Connect(ctx, *pInfo)
 	if err != nil {
-		fmt.Printf("Error : %v\n", err)
+		logger.Errorf("Error: %v\n", err)
+	}
+
+	// wait for subscription to be propagated
+	time.Sleep(time.Second * 2)
+}
+
+func readData(rw *bufio.ReadWriter) {
+	for {
+		str, err := rw.ReadString('\n')
+		if err != nil {
+			logger.Errorf("Error: %v\n", err)
+			panic(err)
+		}
+
+		if str == "" {
+			return
+		}
+		if str != "\n" {
+			// Green console colour: 	\x1b[32m
+			// Reset console colour: 	\x1b[0m
+			logger.Infof("\x1b[32m%s\x1b[0m> ", str)
+		}
+
+	}
+}
+
+func writeData(rw *bufio.ReadWriter) {
+	stdReader := bufio.NewReader(os.Stdin)
+
+	for {
+		logger.Info("> ")
+		sendData, err := stdReader.ReadString('\n')
+		if err != nil {
+			logger.Errorf("Error reading from stdin: %v\n", err)
+			panic(err)
+		}
+
+		_, err = rw.WriteString(fmt.Sprintf("%s\n", sendData))
+		if err != nil {
+			logger.Errorf("Error writing to buffer: %v\n", err)
+			panic(err)
+		}
+		err = rw.Flush()
+		if err != nil {
+			logger.Errorf("Error flushing buffer: %v\n", err)
+			panic(err)
+		}
 	}
 }
